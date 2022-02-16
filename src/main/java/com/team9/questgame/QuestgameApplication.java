ab02f046237516990d248bc8ee15f8bf583ab629
Tom1072 <lamvutungvt@gmail.com>
2022-02-16 17:05:26 -0500

    Initial repo setup

---
+++ src/main/java/com/team9/questgame/QuestgameApplication.java
@@ -0,0 +1,13 @@
+package com.team9.questgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestgameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestgameApplication.class, args);
+	}
+
+}
