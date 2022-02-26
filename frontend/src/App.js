dd8c7021499ce895685b1a5cf05f5a26a4e6efe9
S-Yusuf-Ahmed <yusufyahmed@cmail.carleton.ca>
2022-02-26 15:51:31 -0500

    waiting room  initial commit
    added waiting room and button to test. need api calls to get player list, and ready button should post to start-game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import ConnectForm from "./components/ConnectForm";
 import MessageForm from "./components/MessageForm";
 import Messages from "./components/Messages";
+import WaitingRoom from "./components/WaitingRoom";
 import { useConnected } from "./Store";
 
 const App = () => {
@@ -13,7 +14,8 @@ const App = () => {
       <Header />
       {connected ? (
         <>
-          <MessageForm />
+          <WaitingRoom />
+          {/* <MessageForm /> */}
           <Messages />
         </>
       ) : (
