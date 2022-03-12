71733e3bc96b1a0cdcffa0a7c7f72108c442a453
Tom1072 <lamvutungvt@gmail.com>
2022-03-12 00:34:55 -0500

    Enhance GeneralStateMachine and finished transition to quest phase

--- src/main/java/com/team9/questgame/game_phases/event/EventPhaseController.java
+++ src/main/java/com/team9/questgame/game_phases/event/EventPhaseController.java
@@ -1,4 +1,45 @@
 package com.team9.questgame.game_phases.event;
 
-public class EventPhaseController {
+import com.team9.questgame.Data.PlayerRewardData;
+import com.team9.questgame.Entities.cards.StoryCards;
+import com.team9.questgame.game_phases.GamePhases;
+import com.team9.questgame.game_phases.utils.PlayerTurnService;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EventPhaseController implements GamePhases<StoryCards> {
+    @Override
+    public boolean receiveCard(StoryCards card) {
+        return false;
+    }
+
+    @Override
+    public void discardCard(StoryCards card) {
+
+    }
+
+    @Override
+    public boolean playCard(StoryCards card) {
+        return false;
+    }
+
+    @Override
+    public void onGameReset() {
+
+    }
+
+    @Override
+    public PlayerRewardData getRewards() {
+        return null;
+    }
+
+    @Override
+    public void startPhase(PlayerTurnService playerTurnService) {
+
+    }
+
+    @Override
+    public void onPhaseReset() {
+
+    }
 }
