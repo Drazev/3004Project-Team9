cd9db3018845c625ba39326c49b6a7e97fe9b5e2
Jayson Mendoza <jayson.k.mendoza@gmail.com>
2022-03-18 23:34:02 -0400

    issue 63-1, Jayson Updates to support change to hand and card data. Fixed ability send to a specific user.

--- src/test/java/com/team9/questgame/Entities/cards/AdventureDecksTest.java
+++ src/test/java/com/team9/questgame/Entities/cards/AdventureDecksTest.java
@@ -2,8 +2,9 @@ package com.team9.questgame.Entities.cards;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
-
+@SpringBootTest
 class AdventureDecksTest <T extends Cards> {
 
     private AdventureDecks deck;
