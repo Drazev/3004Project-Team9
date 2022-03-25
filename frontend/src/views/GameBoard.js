f135431af3a2fbc6f12968255e248f0616748812
Johnathan Scaife <johnwscaife@gmail.com>
2022-03-25 12:46:10 -0400

    Compressed PlayerHands
    PlayerHands now renders dynamically using a render function with a for loop.

--- frontend/src/views/GameBoard.js
+++ frontend/src/views/GameBoard.js
@@ -2,7 +2,6 @@ import PlayerHand from "../components/player/PlayerHand";
 import QuestDisplay from "./quest/QuestDisplay";
 import CardImages from "../assets/images/index";
 import Popup from "../components/popups/Popup";
-import Card from "../components/cards/Card";
 import { drawCard, sponsorRespond, setupComplete, participantSetupComplete } from "../services/clientSocket";
 import { useName, usePlayerHands, usePlayers, useTurn, useSponsorRequest, useActivePlayers, useSetPopupType, usePopupType, useIsSponsoring, useSetIsSponsoring, useJoinRequest, useSetJoinRequest, useFoeStageStart, useStoryCard, useHandOversize, useSetFoeStageStart, useNotifyStageStart, useNotifyStageEnd, useNotifyQuestEnd, useSetNotifyStageStart, useSetNotifyStageEnd, useSetNotifyQuestEnd, useNotifyHandOversize, useSetNotifyHandOversize, useNotifyHandNotOversize, useSetNotifyHandNotOversize} from "../stores/generalStore";
 import { useUpdatePlayArea, usePlayerPlayAreas, useStageAreas } from "../stores/playAreaStore";
@@ -38,34 +37,6 @@ function GameBoard(props) {
     const [notifyHandOversize, setNotifyHandOversize] = [useNotifyHandOversize(), useSetNotifyHandOversize()];
     const [notifyHandNotOverSize, setNotifyHandNotOversize] = [useNotifyHandNotOversize(), useSetNotifyHandNotOversize()];
 
-    // useEffect(() => {
-        // if (handOversize) {
-        //     setPopup(true);
-        // }
-        // if (foeStageStart) {
-        //     setFoeStageStartPopup(true);
-        // }
-    // }, [handOversize, foeStageStart])
-
-    // const togglePopup = () => {
-    //     setPopup(!popup);
-    //     console.log("trigger popup: " + popup);
-    // }
-
-    // useEffect(() => {
-    //     setPopupType("SPONSORQUEST")
-    // }, [])
-
-    // useEffect(() => {
-    //     if (joinRequest) {
-    //         setPopupType("JOINQUEST")
-    //     }
-    // }, [joinRequest])
-
-
-    // hands = [{name:"Test1",isTurn:true,hand:[{cardId:1,cardImage:CardImages.Ally_KingArthur}],cardsInPlay:[],rank:CardImages.Rank_Squire,shields:54},
-    // {name:"Test2",isTurn:true,hand:[{cardId:2,cardImage:CardImages.Ally_KingArthur}],cardsInPlay:[],rank:CardImages.Rank_Squire,shields:5}];
-
     let myHandArr = [false, false, false, false];
     let myPlayerID = -1;
     for (let i = 0; i < hands.length; i++) {
@@ -90,71 +61,35 @@ function GameBoard(props) {
             ? active.find(findPlayAreaById(playerId)).cardsInPlay : []
     }
 
+    const renderAllHands = () => {
+        var allHands = [];
+        for (var i = 0; i < hands.length; i++){
+            allHands.push(
+                <PlayerHand
+                    playerName={hands[i].playerName}
+                    playerID={hands[i].playerId}
+                    isTurn={(hands[0].playerName === turn)}
+                    isMyHand={myHandArr[i]}
+                    cardsInHand={hands[i].hand}
+                    activeCards={getActiveCard(hands[i].playerId)}
+                    rank={CardImages.Rank_Squire/*hands[0].rank*/}
+                    numShields={5/*hands[0].shields*/}
+                    top={init + jump*i}
+                    left={i}
+                    shield={CardImages.Shield_3}
+                    numStages={stageAreas.length}
+                ></PlayerHand>
+            );
+        }
+        return <tbody>{allHands}</tbody>;
+    }
+
     return (
         <div id="GameBoard">
             { storyCard && <img src={storyCard.imgSrc} alt="Story Card"/> }
 
             <div id="allHands">
-                <PlayerHand
-                    playerName={hands[0].playerName}
-                    playerID={hands[0].playerId}
-                    isTurn={(hands[0].playerName === turn)}
-                    isMyHand={myHandArr[0]}
-                    cardsInHand={hands[0].hand}
-                    activeCards={getActiveCard(hands[0].playerId)}
-                    rank={CardImages.Rank_Squire/*hands[0].rank*/}
-                    numShields={5/*hands[0].shields*/}
-                    top={init}
-                    left={0}
-                    shield={CardImages.Shield_3}
-                    numStages={stageAreas.length}>
-                </PlayerHand>
-                <PlayerHand
-                    playerName={hands[1].playerName}
-                    playerID={hands[1].playerId}
-                    isTurn={(hands[1].playerName === turn)}
-                    isMyHand={myHandArr[1]}
-                    cardsInHand={hands[1].hand}
-                    activeCards={getActiveCard(hands[1].playerId)}
-                    rank={CardImages.Rank_Squire/*hands[1].rank*/}
-                    numShields={5/*hands[1].shields*/}
-                    top={init + jump}
-                    left={0}
-                    shield={CardImages.Shield_1}
-                    numStages={stageAreas.length}>
-                </PlayerHand>
-                {hands.length > 2 &&
-                    <PlayerHand
-                        playerName={hands[2].playerName}
-                        playerID={hands[2].playerId}
-                        isTurn={(hands[2].playerName === turn)}
-                        isMyHand={myHandArr[2]}
-                        cardsInHand={hands[2].hand}
-                        activeCards={getActiveCard(hands[2].playerId)}
-                        rank={CardImages.Rank_Squire/*hands[2].rank*/}
-                        numShields={5/*hands[2].shields*/}
-                        top={init + jump * 2}
-                        left={0}
-                        numStages={stageAreas.length}
-                        shield={CardImages.Shield_8}>
-                    </PlayerHand>
-                }
-                {hands.length > 3 &&
-                    <PlayerHand
-                        playerName={hands[3].playerName}
-                        playerID={hands[3].playerId}
-                        isTurn={(hands[3].playerName === turn)}
-                        isMyHand={myHandArr[3]}
-                        cardsInHand={hands[3].hand}
-                        activeCards={getActiveCard(hands[3].playerId)}
-                        rank={CardImages.Rank_Squire/*hands[3].rank*/}
-                        numShields={5/*hands[3].shields*/}
-                        top={init + jump * 3}
-                        left={0}
-                        numStages={stageAreas.length}
-                        shield={CardImages.Shield_6}>
-                    </PlayerHand>
-                }
+                {renderAllHands()}
             </div>
             <div className="decks">
                 <img src={CardImages.Back_Adventure} className="deck"></img>
